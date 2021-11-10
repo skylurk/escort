07081fe382dce007557aefae30fcf115d566056a
Skylurk <41697386+skylurk@users.noreply.github.com>
2021-11-10 19:07:31 +0300

    changes

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+    render() {
+        return ( 
+            <Html>
+                <Head>
+
+                {/* Compiled and minified css */}
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+
+                {/* Compiled and minified JavaScrip */}
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+                {/* Materialize icons  */}
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+            
+                    <body>
+                        <Main />
+                        <NextScript />
+                       
+                    </body>
+                </Head>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
