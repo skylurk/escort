a6b2d6c16646d21155659610fdd343e2a1966788
Skylurk <41697386+skylurk@users.noreply.github.com>
2021-11-12 00:51:12 +0300

    changes

---
+++ components/club/SectionOne.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function SectionOne() {
+    return (
+        <div className='club-section-one'>
+            <div className="cso-top">
+                <button className="get-mobile-app">
+                    Get the Mobile App
+                </button>
+                <h4>The Club</h4>
+                <div></div>
+            </div>
+            <div className="cso-mid">
+                <div className="com-left"></div>
+                <div className="com-right"></div>
+            </div>
+            <div className="cso-bottom">
+                <div className="csb-one">
+                    <h5>HOURS & CONTACTS</h5>
+                </div>
+                <div className="csb-two">
+                    <h5>DIRECTIONS</h5>
+                </div>
+                <div className="csb-three">
+                    <h5>PHOTO GALLERY</h5>
+                </div>
+                <div className="csb-four">
+                    <h5>PRICES & SERVICES</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
