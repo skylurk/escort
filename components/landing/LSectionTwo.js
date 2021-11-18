9607e0e2a4bf47f5db2ce38d7bbf5a14e3789ca5
Skylurk <41697386+skylurk@users.noreply.github.com>
2021-11-18 21:50:12 +0300

    changes

--- components/landing/LSectionTwo.js
+++ components/landing/LSectionTwo.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
+import DetailsPage from '../chics/DetailsPage';
 
 export default function LSectionTwo() {
+
+    useEffect(() => {
+        const M = window.M;
+        M.AutoInit();
+    })
     return (
         <div className='l-section-two'>
             <div className="text-area">
@@ -15,11 +21,26 @@ export default function LSectionTwo() {
                 </div>
                 </a>
             </Link>
-            <div className="lst-two">
-                <img src="/assets/images/landing_02.png" alt="" />
-            </div>
+
+            <a href="#modal1" className="modal-trigger">
+                <div className="lst-two">
+                    <img src="/assets/images/landing_02.png" alt="" />
+                </div>
+            </a>
+
             <div className="lst-three">
                 <img src="/assets/images/slider_03.png" alt="" />
+            </div>
+
+            <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+
+            <div id="modal1" className="modal">
+            <div class="modal-content">
+                <DetailsPage />
+                {/* <h4>Modal Header</h4>
+                <p>A bunch of text</p> */}
+            </div>
+
             </div>
         </div>
     )
